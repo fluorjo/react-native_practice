754deace64642ca1c3cb9697cb2a072accf38624
fluorjo <fluorjo@naver.com>
2023-10-02 17:20:39 +0900

    조건문 축약.
    에러 화면 안 뜨게.

--- App.tsx
+++ App.tsx
@@ -5,31 +5,25 @@ import React from 'react';
 
 import {Text, SafeAreaView} from 'react-native';
 
-console.error = error => error.apply;
+import {LogBox} from 'react-native';
 
-//DOM 객체를 네이티브로 넘긴다.
 export default function App() {
-  //가상 DOM 객체 생성
+  console.error = error => error.apply;
+  LogBox.ignoreAllLogs();
+  
   const textElement = React.createElement(Text, null, '가상 DOM 객체');
+
   const virtualDOM = (
     <SafeAreaView>
       <Text>DOM 자체를 변수로 만들 수도 있다.</Text>
     </SafeAreaView>
   );
-  const isLoading = true;
-  if (isLoading) {
-    return (
-      <SafeAreaView>
-        <Text>이 부분은 isLoading이 true일 때만 나온다. </Text>
-        <Text>이렇게 바로 적어줄 수도 있고</Text>
-        <Text>{textElement}를 변수처럼 불러올 수도 있다.</Text>
-        {virtualDOM}
-      </SafeAreaView>
-    );
-  }
+
+  const isLoading = false;
   return (
     <SafeAreaView>
-      <Text>이 부분은 isLoading이 false일 때만 나온다. </Text>
+      {isLoading && <Text>isLoading이 true일 때만 나온다. </Text>}
+      {!isLoading && <Text>isLoading이 false일 때만 나온다. </Text>}
     </SafeAreaView>
   );
 }
