bf1c195d91355dfa56b5d0c5cac4e54631a55bb4
fluorjo <fluorjo@naver.com>
2023-10-04 00:09:57 +0900

    styles, colors

--- App.tsx
+++ App.tsx
@@ -1,39 +1,30 @@
 //에러 화면 방지
-// console.error = error => error.apply;
-// LogBox.ignoreAllLogs();
-// LogBox.ignoreLogs(['Property ']);
-// LogBox.ignoreLogs(['it is un']);
-// LogBox.ignoreLogs(['The title']);
-// LogBox.ignoreLogs(['']);
+console.error = error => error.apply;
+LogBox.ignoreAllLogs();
+LogBox.ignoreLogs(['Property ']);
+LogBox.ignoreLogs(['it is un']);
+LogBox.ignoreLogs(['The title']);
+LogBox.ignoreLogs(['']);
 //
 //JSX 구문을 쓰려면 이 import가 필요.
 import React from 'react';
-
-import {Text, SafeAreaView, Button, Alert} from 'react-native';
-import {TouchableOpacity, TouchableHighlight} from 'react-native';
 import {LogBox, View} from 'react-native';
-import {TextInput} from 'react-native';
 
-const onPress = () => Alert.alert('home pressed.', 'message');
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
+
+import {MD2Colors as Colors} from 'react-native-paper';
+
+const styles = StyleSheet.create({
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center',
+  backgroundColor: Colors.blue600},
+  text: {fontSize: 20, color: Colors.blue100}
+  })
+  
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Button title="home" onPress={onPress} />
-      <TouchableOpacity onPress={onPress}>
-        <Text>TouchableOpacity</Text>
-      </TouchableOpacity>
-      <TouchableHighlight onPress={onPress}>
-        <Text>TouchableHighlight</Text>
-      </TouchableHighlight>
-      <Text onPress={onPress}>text</Text>
-      <TextInput
-        placeholder="enter your name"
-        onChangeText={(text: string) => console.log(text)}
-        onFocus={() => console.log('onFocus')}
-        onBlur={() => console.log('onBlur')}
-        onEndEditing={() => console.log('onEndEditing')}
-      />
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style= {[styles.text]}>그리워하면 언젠가 만나게 되는 어느 영화와 같은 일들이 이뤄져 가기를</Text>
     </SafeAreaView>
   );
 }
