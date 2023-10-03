bfbb79e4625cfbd27b6ffa8a0b0854a988536e8b
fluorjo <fluorjo@naver.com>
2023-10-03 18:26:24 +0900

    reset

--- App.tsx
+++ App.tsx
@@ -7,8 +7,6 @@ import {Text, SafeAreaView} from 'react-native';
 
 import {LogBox, View} from 'react-native';
 
-import * as D from './src/data';
-
 //에러 화면 방지
 console.error = error => error.apply;
 LogBox.ignoreAllLogs();
@@ -16,13 +14,10 @@ LogBox.ignoreLogs(['Property ']);
 LogBox.ignoreLogs(['it is un']);
 //
 
-const person = D.createRandomPerson();
 
 export default function App() {
   return (
-    <SafeAreaView>
-      {/* null, 2는 공백 문자 두 개 넣는다는 뜻. */}
-      <Text>{JSON.stringify(person, null, 2)}</Text>
-    </SafeAreaView>
+    <SafeAreaView/>
+
   );
 }
