be59b46cd68a572038b87369f6641517af446340
fluorjo <fluorjo@naver.com>
2023-10-04 09:04:09 +0900

    style. flex, height

--- App.tsx
+++ App.tsx
@@ -14,17 +14,26 @@ import {Text, SafeAreaView, StyleSheet} from 'react-native';
 
 import {MD2Colors as Colors} from 'react-native-paper';
 
+import {Platform, Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center',
-  backgroundColor: Colors.white},
-  text: {fontSize: 20, color: Colors.blue900}
-  })
-  
+  safeAreaView: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.green200,
+    height: 40,
+  },
+  text: {fontSize: 20, color: Colors.blue900},
+});
 
 export default function App() {
   return (
     <SafeAreaView style={[styles.safeAreaView]}>
-      <Text style= {[styles.text]}>와 이제 좀 살 거 같다. </Text>
+      <Text style={[styles.text]}>와 이제 좀 살 거 같다. </Text>
+      <Text style={[styles.text]}>height:{height} </Text>
     </SafeAreaView>
   );
 }
