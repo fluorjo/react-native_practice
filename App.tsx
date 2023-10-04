50def108e788c4be3be07b4854e4f7c39b60525c
fluorjo <fluorjo@naver.com>
2023-10-04 20:02:45 +0900

    3-3

--- App.tsx
+++ App.tsx
@@ -12,41 +12,10 @@ import {LogBox, View} from 'react-native';
 
 import {Text, SafeAreaView, StyleSheet} from 'react-native';
 
-import {MD2Colors as Colors} from 'react-native-paper';
-
-import {Platform, Dimensions} from 'react-native';
-
-const {width, height} = Dimensions.get('window');
+export default function App() {
+  return;
+}
 
 const styles = StyleSheet.create({
-  safeAreaView: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: Colors.green200,
-    height: 40,
-    //이거 ios에서는 안 먹힘.
-    padding:100
-
-  },
-  text: {fontSize: 20, color: Colors.blue900},
-  box: {
-    width: '70%',
-    height: 100,
-    backgroundColor: 'white',
-    marginBottom: 10,
-    //플랫폼에 따라 값을 다르게 해준다. 
-    marginLeft: Platform.select({ios: 20, android: 0}),
-  },
+  flex: {flex: 1},
 });
-
-export default function App() {
-  return (
-    <SafeAreaView style={[styles.safeAreaView]}>
-      <Text style={[styles.text]}>와 이제 좀 살 거 같다. </Text>
-      <Text style={[styles.text]}>height:{height} </Text>
-      <View style={[styles.box, {borderRadius: 10}]}/>  
-
-    </SafeAreaView>
-  );
-}
