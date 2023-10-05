869e0437774c20af8a6f58c613c2cfc6b898089e
fluorjo <fluorjo@naver.com>
2023-10-05 18:37:40 +0900

    3.4. 준비

--- App.tsx
+++ App.tsx
@@ -20,46 +20,23 @@ import {
 
 import * as D from './src/data';
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import {MD2Colors as Colors} from 'react-native-paper';
 
-const text = `Queen.`;
-
-const onIconPressed=()=>console.log('icon pressed')
+import TopBar from './src/screens/TopBar';
+import Content from './src/screens/Content';
+import BottomBar from './src/screens/BottomBar';
 
 export default function App() {
   return (
     <SafeAreaView style={[styles.flex]}>
-      <ImageBackground
-        style={[styles.flex]}
-        source={require('./src/assets/images/bg.jpg')}>
-        <Image source={{uri: avatarUrl}} style={[styles.image]} />
-        <View style={[styles.flex, styles.padding10]}>
-          <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
-          <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
-          <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
-          <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
-        </View>
-        <Icon name='home' size={50}color={Colors.black} onPress={onIconPressed}></Icon>
-      </ImageBackground>
+      <TopBar />
+      <Content />
+      <BottomBar />
     </SafeAreaView>
   );
 }
-const avatarUrl = D.randomAvatarUrl();
-const avatarSize = 50;
 const styles = StyleSheet.create({
   flex: {flex: 1},
-  ImageBackground: {padding: 10},
-  padding10: {padding: 10},
-
-  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
-  text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
-  regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'},
-  medium: {fontFamily: 'DancingScript-Medium', fontWeight: '500'},
-  semiBold: {fontFamily: 'DancingScript-SemiBold', fontWeight: '600'},
-  bold: {
-    fontFamily: 'DancingScript-Bold',
-    fontWeight: Platform.select({ios: '700', android: '600'}),
-  },
 });
