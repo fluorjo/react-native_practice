c6a0f9f3078e07b29b24578887d406856e329f25
fluorjo <fluorjo@naver.com>
2023-10-05 14:27:36 +0900

    text

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import {
 } from 'react-native';
 
 import * as D from './src/data';
+const text = `Almost before we knew it, we had left the ground.`;
 
 export default function App() {
   return (
@@ -27,6 +28,12 @@ export default function App() {
         style={[styles.flex]}
         source={require('./src/assets/images/bg.jpg')}>
         <Image source={{uri: avatarUrl}} style={[styles.image]} />
+        <View style={[styles.flex, styles.padding10]}>
+          <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
+          <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
+          <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
+          <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
+        </View>
       </ImageBackground>
     </SafeAreaView>
   );
@@ -36,6 +43,8 @@ const avatarSize = 50;
 const styles = StyleSheet.create({
   flex: {flex: 1},
   ImageBackground: {padding: 10},
+  padding10: {padding: 10},
+
   image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
   text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
   regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'},
