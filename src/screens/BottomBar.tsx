b5072de554c9f909f01432dac5ccf9c4344a46ae
fluorjo <fluorjo@naver.com>
2023-10-06 04:29:05 +0900

    bottombar,content

--- src/screens/BottomBar.tsx
+++ src/screens/BottomBar.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+//import {Colors} from 'react-native-paper'
 import {MD2Colors as Colors} from 'react-native-paper';
-import * as D from '../data';
-
-const title = 'BottomBar';
-export default function CopyMe() {
-  return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
-    </View>
-  );
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+const iconSize = 30,
+  iconColor = 'white';
+const icons = [
+  'home',
+  'table-search',
+  // 'face-profile-woman',
+  'account-cowboy-hat',
+  'account-settings',
+];
+export default function BottomBar() {
+  const children = icons.map(name => (
+    <Icon key={name} name={name} size={iconSize} color={iconColor} />
+  ));
+  return <View style={styles.view}>{children}</View>;
 }
 const styles = StyleSheet.create({
-  view: {padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    padding: 10,
+    backgroundColor: Colors.lightBlue500,
+  },
 });
--- src/screens/Content.tsx
+++ src/screens/Content.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import {MD2Colors as Colors} from 'react-native-paper';
+import {StyleSheet, View, Image, ScrollView} from 'react-native';
 import * as D from '../data';
-
-const title = 'Content';
-export default function CopyMe() {
-  return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
-      <View style={[{flex: 0, backgroundColor: Colors.amber100}]}>
-        <Text>flex 0</Text>
-      </View>
-      <View style={[{flex: 1, backgroundColor: Colors.amber300}]}>
-        <Text>flex 1</Text>
-      </View>
-      <View style={[{flex: 2, backgroundColor: Colors.amber500}]}>
-        <Text>flex 2</Text>
-      </View>
+const avatars = D.makeArray(200).map(notUsed => D.randomAvatarUrl());
+export default function Content() {
+  const children = avatars.map((avatarUrl, index) => (
+    <View key={index.toString()} style={styles.avatarView}>
+      <Image style={styles.avatar} source={{uri: avatarUrl}} />
     </View>
+  ));
+  return (
+    <ScrollView contentContainerStyle={[styles.view]}>{children}</ScrollView>
   );
 }
 const styles = StyleSheet.create({
-  view: {padding: 5, backgroundColor: Colors.blue900, flex: 1, height: '100%'},
-  text: {fontSize: 20, color: 'white'},
+  view: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    // flex: 1,
+    padding: 5,
+  },
+  text: {fontSize: 20},
+  avatarView: {padding: 3},
+  avatar: {width: 50, height: 50, borderRadius: 25},
 });
