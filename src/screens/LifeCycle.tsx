d2252058bc45747da885d8e2918e3373dd1bf83a
fluorjo <fluorjo@naver.com>
2023-10-08 03:50:37 +0900

    timer

--- src/screens/LifeCycle.tsx
+++ src/screens/LifeCycle.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useEffect, useLayoutEffect, useCallback} from 'react';
+import {Platform, StyleSheet, View, Text} from 'react-native';
+//import {Colors} from 'react-native-paper'
 import {MD2Colors as Colors} from 'react-native-paper';
-import * as D from '../data';
+import type {LayoutChangeEvent} from 'react-native';
 
-const title = 'LifeCycle';
-export default function CopyMe() {
+export default function LifeCycle() {
+  useEffect(() => {
+    console.log(Platform.OS, 'useEffect called');
+    return () => console.log(Platform.OS, 'useEffect finished');
+  }, []);
+  useLayoutEffect(() => {
+    console.log(Platform.OS, 'useLayoutEffect called');
+    return () => console.log(Platform.OS, 'useLayoutEffect finished');
+  }, []);
+  const onLayout = useCallback((e: LayoutChangeEvent) => {
+    const {layout} = e.nativeEvent;
+    console.log(Platform.OS, 'onLayout called', layout);
+  }, []);
+  console.log(Platform.OS, 'render start');
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+    <View onLayout={onLayout} style={styles.view}>
+      <Text style={styles.title}>LifeCycle</Text>
     </View>
   );
 }
+// prettier-ignore
 const styles = StyleSheet.create({
-  view: {padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
-});
+  view: {flex: 1, alignItems: 'center', backgroundColor: Colors.blue100},
+  title: {fontSize: 30, fontWeight: '600'}
+  })
