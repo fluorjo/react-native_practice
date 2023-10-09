970216c280c8192cef3ec761e39719908993685e
fluorjo <fluorjo@naver.com>
2023-10-09 09:54:16 +0900

    Country.tsx

--- src/screens/Country.tsx
+++ src/screens/Country.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import {MD2Colors as Colors} from 'react-native-paper';
-import * as D from '../data';
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+import type {FC} from 'react'
+import * as D from '../data'
 
-const title = 'Country';
-export default function CopyMe() {
-  return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
-    </View>
-  );
+export type CountryProps = {
+  country: D.ICountry
 }
+const Country: FC<CountryProps> = ({country}) => {
+  const {name, population, subregion, region} = country
+  return (
+    <View style={styles.view}>
+      <View>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View>
+        <Text>population: {population}</Text>
+        <Text>subregion: {subregion}</Text>
+        <Text>region: {region}</Text>
+      </View>
+    </View>
+  )
+}
+export default Country
 const styles = StyleSheet.create({
-  view: {padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
-});
+  view: {padding: 5},
+  name: {fontSize: 30, fontWeight: '400'}
+})
