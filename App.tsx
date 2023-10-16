63a97462ed9a21b0db2144111b31b3070ddcdbd2
fluorjo <fluorjo@naver.com>
2023-10-16 14:41:44 +0900

    memo

--- App.tsx
+++ App.tsx
@@ -20,10 +20,12 @@ type RootStackParamList = {
   Home: undefined;
   Details: undefined;
 };
+//밑에서 쓸 타입을 만들어준 것.
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
-//{navigation}<-이게 매개변수. 이거의 타입이 HomeScreenprops라고 선언.
+//{navigation}<-이게 매개변수. 이거의 타입이 :HomeScreenprops라고 선언.(타입스크립트라 필요한 것.)
+
 function HomeScreen({navigation}: HomeScreenProps) {
   const onClick = useCallback(() => {
     navigation.navigate('Details');
@@ -31,13 +33,25 @@ function HomeScreen({navigation}: HomeScreenProps) {
 
   return (
     <>
-          <View style={{flex: 1, backgroundColor: 'yellow' ,alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableHighlight onPress={onClick}>
-              <Text>Home Screen</Text>
-            </TouchableHighlight>
-          </View>
-          <View style={{flex: 2, backgroundColor: 'green' ,alignItems: 'center', justifyContent: 'center'}}></View>
-          </>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'yellow',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <TouchableHighlight onPress={onClick}>
+          <Text>Home Screen</Text>
+        </TouchableHighlight>
+      </View>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: 'green',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}></View>
+    </>
   );
 }
 
@@ -47,30 +61,36 @@ function DetailsScreen({navigation}: DetailsScreenProps) {
   }, [navigation]);
 
   return (
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableHighlight onPress={onClick}>
-              <Text>Details Screen</Text>
-            </TouchableHighlight>
-          </View>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>Details Screen</Text>
+      </TouchableHighlight>
+    </View>
   );
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 function App() {
   return (
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen
-                      name="Home"
-                      component={HomeScreen}
-                      options={{title: 'Overview'}}
-              />
-              <Stack.Screen name="Details">
-                {props => <DetailsScreen {...props} />}
-              </Stack.Screen>
-            </Stack.Navigator>
-          </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details">
+          {props => <DetailsScreen {...props} />}
+          {/* 스크린이 컴포넌트에게 네비게이션과 라우트를 전달한다. */}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 export default App;
+//{(props:{navigation:any,route:RouteProp<ParamListBase,RouteName})
+//여기서 props:이후를 다 줄인 것.
+//<DetailsScreen navigation={}route={}}
+//navigation={}route={}<-이거를 줄여쓴 게 {...props}
+//navigation, route를 넣어줘야 하는 걸 줄여쓴 것.
