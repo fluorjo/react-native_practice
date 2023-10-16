d11f115d1f4601c4900d31b7537a89bc729ce2f7
fluorjo <fluorjo@naver.com>
2023-10-17 04:39:56 +0900

    1-2

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, { useState } from 'react';
+import {Pressable, Text, View} from 'react-native';
 
 function Complete() {
+  
   return (
     <View>
-      <Text>완료 화면</Text>
+<Text>완료화면</Text>
     </View>
   );
 }
--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import React from 'react'
+import {Text, View} from 'react-native'
+import Complete from './Complete'
+import Ing from './Ing'
+
+const Stack = createNativeStackNavigator()
 
 function Delivery() {
   return (
-    <View>
-      <Text>배달 화면</Text>
-    </View>
-  );
+    <Stack.Navigator initialRouteName="Ing">
+      <Stack.Screen name="Ing" component={Ing} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Complete"
+        component={Complete}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  )
 }
 
-export default Delivery;
+export default Delivery
