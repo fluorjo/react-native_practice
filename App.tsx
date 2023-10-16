f05df9c0d440b1e1bc55ea3a730dc84500c69531
fluorjo <fluorjo@naver.com>
2023-10-17 03:24:20 +0900

    1-1.앱 구성.

--- App.tsx
+++ App.tsx
@@ -1,97 +1,67 @@
-//에러 화면 방지
-import {LogBox} from 'react-native';
-console.error = error => error.apply;
-LogBox.ignoreAllLogs();
-LogBox.ignoreLogs(['Property ']);
-LogBox.ignoreLogs(['it is un']);
-LogBox.ignoreLogs(['The title']);
-LogBox.ignoreLogs(['']);
-//
 import * as React from 'react';
-import {NavigationContainer, ParamListBase} from '@react-navigation/native';
-import {
-  createNativeStackNavigator,
-  NativeStackScreenProps,
-} from '@react-navigation/native-stack';
-import {Text, TouchableHighlight, View} from 'react-native';
-import {useCallback} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Settings from './src/pages/Settings';
+import Orders from './src/pages/Orders';
+import Delivery from './src/pages/Delivery';
+import {useState} from 'react';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
-type RootStackParamList = {
-  //페이지 목록과 parameter를 넣는다. 넣을 거 없으면 undefined.
-  Home: undefined;
-  Details: undefined;
+export type LoggedInParamList = {
+  Orders: undefined;
+  Settings: undefined;
+  Delivery: undefined;
+  Complete: {orderId: string};
 };
-//밑에서 쓸 타입을 만들어준 것.
-type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
-type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
-//{navigation}<-이게 매개변수. 이거의 타입이 :HomeScreenprops라고 선언.(타입스크립트라 필요한 것.)
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
 
-function HomeScreen({navigation}: HomeScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Details');
-  }, [navigation]);
-
-  return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'yellow',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <TouchableHighlight onPress={onClick}>
-          <Text>Home Screen</Text>
-        </TouchableHighlight>
-      </View>
-      <View
-        style={{
-          flex: 2,
-          backgroundColor: 'green',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}></View>
-    </>
-  );
-}
-
-function DetailsScreen({navigation}: DetailsScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Home');
-  }, [navigation]);
-
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <TouchableHighlight onPress={onClick}>
-        <Text>Details Screen</Text>
-      </TouchableHighlight>
-    </View>
-  );
-}
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Overview'}}
-        />
-        <Stack.Screen name="Details">
-          {props => <DetailsScreen {...props} />}
-          {/* 스크린이 컴포넌트에게 네비게이션과 라우트를 전달한다. */}
-        </Stack.Screen>
-      </Stack.Navigator>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
 
 export default App;
-//{(props:{navigation:any,route:RouteProp<ParamListBase,RouteName})
-//여기서 props:이후를 다 줄인 것.
-//<DetailsScreen navigation={}route={}}
-//navigation={}route={}<-이거를 줄여쓴 게 {...props}
-//navigation, route를 넣어줘야 하는 걸 줄여쓴 것.
