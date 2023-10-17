8f80ea304f7f159f9499d8462828febac020a566
fluorjo <fluorjo@naver.com>
2023-10-17 20:00:15 +0900

    1-4

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,19 +1,37 @@
-import React, {useCallback, useState} from 'react'
+import {NativeStackScreenProps} from '@react-navigation/native-stack'
+import React, {useCallback, useRef, useState} from 'react'
 import {Alert, Pressable, StyleSheet, Text, TextInput, View} from 'react-native'
+import {RootStackParamList} from '../../App'
 
-function SignIn() {
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>
+
+function SignIn({navigation}: SignInScreenProps) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
   const onSubmit = useCallback(() => {
-    Alert.alert('알림', '버튼 클릭')
-  }, [])
+    if (!email || !email.trim()) {
+      return Alert.alert('알림', '이메일을 입력해주세요')
+    }
+    if (!password || !password.trim()) {
+      return Alert.alert('알림', '비밀번호를 입력해주세요')
+    }
+    Alert.alert('알림', '로그인 되었습니다.')
+  }, [email, password])
   const onChangeEmail = useCallback((text: React.SetStateAction<string>) => {
     setEmail(text)
   }, [])
   const onChangePassword = useCallback((text: React.SetStateAction<string>) => {
     setPassword(text)
   }, [])
+
+  const toSignUp = useCallback(() => {
+    navigation.navigate('SignUp')
+  }, [navigation])
+
   const canGoNext = email && password
+  const emailRef = useRef<TextInput | null>(null)
+  const passwordRef = useRef<TextInput | null>(null)
 
   return (
     <>
@@ -22,14 +40,38 @@ function SignIn() {
         <TextInput
           style={styles.textInput}
           placeholder="이메일을 입력해주세요"
-          onChangeText={onChangeEmail}></TextInput>
+          onChangeText={onChangeEmail}
+          value={email}
+          importantForAutofill="yes"
+          autoComplete="email"
+          textContentType="emailAddress"
+          returnKeyType="next"
+          onSubmitEditing={() => {
+            passwordRef.current?.focus()
+          }}
+          blurOnSubmit={false}
+          ref={emailRef}
+          clearButtonMode="while-editing" //아이폰. 클리어 버튼.
+          keyboardType="email-address"
+        />
       </View>
       <View style={styles.inputWrapper}>
         <Text style={styles.label}>비밀번호</Text>
         <TextInput
           style={styles.textInput}
           placeholder="비밀번호를 입력해주세요"
-          onChangeText={onChangePassword}></TextInput>
+          onChangeText={onChangePassword}
+          value={password}
+          importantForAutofill="yes"
+          autoComplete="password"
+          textContentType="password"
+          ref={passwordRef}
+          onSubmitEditing={() => {
+            onSubmit
+          }}
+          keyboardType="email-address"
+          secureTextEntry
+        />
       </View>
       <View style={styles.buttonZone}>
         <Pressable
@@ -42,7 +84,7 @@ function SignIn() {
           }>
           <Text style={styles.loginButtonText}>로그인</Text>
         </Pressable>
-        <Pressable onPress={onSubmit} style={styles.loginButton}>
+        <Pressable onPress={toSignUp} style={styles.loginButton}>
           <Text style={styles.loginButtonText}>회원가입</Text>
         </Pressable>
       </View>
